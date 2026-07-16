aa403777570c0740c2325a862c82d40bb090d952
samuel-kwasi-boahen <kwasisamuelboahen@gmail.com>
2026-07-16 16:23:16 +0000

    feat: create simple shopping calculator

--- 07-javascript-fundamentals/script.js
+++ 07-javascript-fundamentals/script.js
@@ -11,3 +11,19 @@ console.log("profession:", profession);
 console.log("yearLearning:", yearLearning);
 console.log("enjoysCoding:", enjoysCoding);
 console.log("isLearningJS:", isLearningJS);
+
+let productName = "Notebooks";
+let price = 25;
+let quantity = 4;
+
+let totalCost = price * quantity;
+console.log("--- Product Info ---");
+console.log("productName:", productName);
+console.log("price:", price);
+console.log("quantity:", quantity);
+console.log("totalCost:", totalCost);
+
+let customerName = "Samuel Boahen";
+console.log("customerName:", customerName);
+let message = customerName + " bought " + quantity + " " + productName + " for GHS" + totalCost;
+console.log(message);
