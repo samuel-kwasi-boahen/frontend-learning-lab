f53fa4b68c9b45d73b074e40aa4ce361edf87f40
samuel-kwasi-boahen <kwasisamuelboahen@gmail.com>
2026-07-16 09:18:38 +0000

    feat: create developer profile logger with variables

--- 07-javascript-fundamentals/script.js
+++ 07-javascript-fundamentals/script.js
@@ -1,1 +1,13 @@
-        alert("Hello! Welcome to JavaScript Fundamentals.");
+        //alert("Hello! Welcome to JavaScript Fundamentals.");
+let fullName = "Samuel Boahen";
+let profession = "frontend Engineer";
+let yearLearning = 1;
+let enjoysCoding = true;
+let isLearningJS = true;
+
+console.log("--- Developer Info ---");
+console.log("fullName:", fullName);
+console.log("profession:", profession);
+console.log("yearLearning:", yearLearning);
+console.log("enjoysCoding:", enjoysCoding);
+console.log("isLearningJS:", isLearningJS);
