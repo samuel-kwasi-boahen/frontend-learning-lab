968d214e1f1237a7a94bf72fc0e4b159224253e2
samuel-kwasi-boahen <kwasisamuelboahen@gmail.com>
2026-07-16 17:26:27 +0000

    feat: build student eligibility checker using comparison operators

--- 07-javascript-fundamentals/script.js
+++ 07-javascript-fundamentals/script.js
@@ -1,29 +1,58 @@
-        //alert("Hello! Welcome to JavaScript Fundamentals.");
-let fullName = "Samuel Boahen";
-let profession = "frontend Engineer";
-let yearLearning = 1;
-let enjoysCoding = true;
-let isLearningJS = true;
+//         //alert("Hello! Welcome to JavaScript Fundamentals.");
+// let fullName = "Samuel Boahen";
+// let profession = "frontend Engineer";
+// let yearLearning = 1;
+// let enjoysCoding = true;
+// let isLearningJS = true;
 
-console.log("--- Developer Info ---");
-console.log("fullName:", fullName);
-console.log("profession:", profession);
-console.log("yearLearning:", yearLearning);
-console.log("enjoysCoding:", enjoysCoding);
-console.log("isLearningJS:", isLearningJS);
+// console.log("--- Developer Info ---");
+// console.log("fullName:", fullName);
+// console.log("profession:", profession);
+// console.log("yearLearning:", yearLearning);
+// console.log("enjoysCoding:", enjoysCoding);
+// console.log("isLearningJS:", isLearningJS);
 
-let productName = "Notebooks";
-let price = 25;
-let quantity = 4;
+// let productName = "Notebooks";
+// let price = 25;
+// let quantity = 4;
 
-let totalCost = price * quantity;
-console.log("--- Product Info ---");
-console.log("productName:", productName);
-console.log("price:", price);
-console.log("quantity:", quantity);
-console.log("totalCost:", totalCost);
+// let totalCost = price * quantity;
+// console.log("--- Product Info ---");
+// console.log("productName:", productName);
+// console.log("price:", price);
+// console.log("quantity:", quantity);
+// console.log("totalCost:", totalCost);
 
-let customerName = "Samuel Boahen";
-console.log("customerName:", customerName);
-let message = customerName + " bought " + quantity + " " + productName + " for GHS" + totalCost;
-console.log(message);
+// let customerName = "Samuel Boahen";
+// console.log("customerName:", customerName);
+// let message = customerName + " bought " + quantity + " " + productName + " for GHS" + totalCost;
+// console.log(message);
+
+/*let apples = 5;
+
+let oranges = 3;
+
+let fruits = apples + oranges;
+
+
+apples = 10;
+
+console.log(fruits);*/
+
+let studentName = "Samuel"
+let age = 22;
+let minimumAge = 18;
+
+console.log("---Student Eligibility Checker---")
+
+console.log(studentName)
+console.log("The student is older than the minimum age: " +( age > minimumAge));
+
+console.log("The student is older than or equal to the minimum age: " + (age >= minimumAge));
+
+console.log("The student is less than the minimum age: " + (age < minimumAge));
+
+console.log("The student is exactly the minimum age: " + (age === minimumAge));
+
+console.log("The student is not equal to the minimum age: "+ (age != minimumAge));
+
